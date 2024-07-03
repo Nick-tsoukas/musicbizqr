ae0dacae8d9a90b38806a20af60a6b61ed03caa8
Nick-tsoukas <nick.tsoukas101@gmail.com>
2024-07-02 18:20:22 -0700

    chore: dashboard page

--- components/MaterialCard.vue
+++ components/MaterialCard.vue
@@ -1,23 +1,44 @@
 <template>
   <div class="max-w-sm rounded-lg overflow-hidden shadow-lg m-4 border border-gray-200">
-    <img class="w-full h-48 object-cover" src="https://via.placeholder.com/300" alt="Card Image">
+    <img class="w-full h-48 object-cover" :src="q_image" alt="Card Image">
     <div class="p-4">
-      <h2 class="font-bold text-xl mb-2">Card Title</h2>
-      <p class="text-gray-700 text-base">This is supporting text. It provides additional information about the content of the card.</p>
+      <h2 class="font-bold text-xl mb-2">{{name}}</h2>
+      <p class="text-gray-700 text-base">{{ url }}</p>
     </div>
-    <div class="px-4 py-2 flex justify-start space-x-2">
-      <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">Action 1</button>
-      <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">Action 2</button>
-      <img src="edit-icon.svg" alt="">
-      <img src="delete-icon.svg" alt="">
+    <div class="px-4 py-2 flex w-full justify-start space-x-2">
+      <button class="bg-blue-500 text-white font-bold py-2 px-4 mr-auto rounded hover:bg-blue-700">Download QR</button>
+   
+      <img src="@/assets/edit-icon.svg" alt="Edit Icon" class="icon ml-auto" />
+      <img src="@/assets/delete-icon.svg" alt="Delete Icon" class="icon" />
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'MaterialCard'
-}
+<script setup>
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'Default Title'
+  },
+  url: {
+    type: String,
+    default: ''
+  },
+  q_image: {
+    type: String,
+    default: 'https://via.placeholder.com/300'
+  },
+  scanTime: {
+    type: Array,
+    default: () => []
+  },
+  link: {
+    type: String,
+    default: null
+  }
+})
+
 </script>
 
 <style scoped>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,37 @@
-<script lang="ts" setup>
+<script  setup>
+const { find } = useStrapi()
+const user = useStrapiUser()
 
+const qrs = ref([])
+async function fetchQrCodes() {
+  if (user.value) {
+    try {
+      const response = await find('qrs', {
+        filters: {
+          users_permissions_user: {
+            id: {
+              $eq: user.value.id
+            }
+          }
+        },
+        populate: '*'
+      })
+      qrs.value = response.data
+    } catch (error) {
+      console.error('Error fetching QR codes:', error)
+    }
+  }
+}
+
+fetchQrCodes()
 </script>
 
 <template>
   <div>
-   
-    <MaterialCard />
+=
+    <div v-if="qrs.length">
+      <MaterialCard v-for="qr in qrs" :key="qr.name" :name="qr.attributes.name" :q_image="qr.attributes.q_image.data.attributes.url" />
+    </div>
   </div>
 </template>
 
