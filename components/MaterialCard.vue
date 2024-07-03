d5bb0802eb434d1e7f9bdc80db08b94e391ce838
Nick-tsoukas <nick.tsoukas101@gmail.com>
2024-07-02 17:17:52 -0700

    chore: adding icons

--- components/MaterialCard.vue
+++ components/MaterialCard.vue
@@ -5,9 +5,11 @@
       <h2 class="font-bold text-xl mb-2">Card Title</h2>
       <p class="text-gray-700 text-base">This is supporting text. It provides additional information about the content of the card.</p>
     </div>
-    <div class="px-4 py-2 flex justify-end space-x-2">
+    <div class="px-4 py-2 flex justify-start space-x-2">
       <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">Action 1</button>
       <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">Action 2</button>
+      <img src="edit-icon.svg" alt="">
+      <img src="delete-icon.svg" alt="">
     </div>
   </div>
 </template>
