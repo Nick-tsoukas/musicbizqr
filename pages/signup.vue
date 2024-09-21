ee9e8710df57c6583bfd21ec151f3e1197fb7952
Nick-tsoukas <nick.tsoukas101@gmail.com>
2024-09-21 11:16:59 -0700

    chore: signup

--- pages/signup.vue
+++ pages/signup.vue
@@ -8,7 +8,7 @@ const [password, passwordAttrs] = defineField('password')
 
 const signUp = async () => {
   try {
-    console.log(values.emai, values.passwordl)
+    console.log(values.emaail, values.password)
     await register({ username: values.email, email: values.email, password: values.password})
     router.push('/dashboard')
   } catch (e) {
