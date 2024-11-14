f051f16a4440fd6dd7101daff47747f96d128c78
Nick-tsoukas <nick.tsoukas101@gmail.com>
2024-11-14 15:46:54 -0800

    chore: ...

--- pages/directqr.vue
+++ pages/directqr.vue
@@ -28,29 +28,30 @@ onMounted(async () => {
 
     if (qrData && qrData.length > 0) {
       const qr = qrData[0]; // Assuming you get one QR code per UUID
-
+console.log(qrData , 'this is qr data')
       // Check the QR type and perform the appropriate redirection
       if (qr.q_type === 'bandProfile' && qr.band) {
         // Redirect to the band profile page
-        router.push({ name: 'band', params: { id: qr.band.id } });
+        // router.push({ name: 'band', params: { id: qr.band.id } });
       } else if (qr.q_type === 'events' && qr.event) {
         // Redirect to the event page
-        router.push({ name: 'event', params: { id: qr.event.id } });
+        // router.push({ name: 'event', params: { id: qr.event.id } });
       } else if (qr.q_type === 'tours' && qr.tour) {
         // Redirect to the tour page
-        router.push({ name: 'tour', params: { id: qr.tour.id } });
+        // router.push({ name: 'tour', params: { id: qr.tour.id } });
       } else if (qr.q_type === 'albums' && qr.album) {
         // Redirect to the album page
-        router.push({ name: 'album', params: { id: qr.album.id } });
+        // router.push({ name: 'album', params: { id: qr.album.id } });
       } else if (qr.q_type === 'stream' && qr.link) {
         // Redirect to an external streaming link
-        window.location.href = qr.link;
+        // window.location.href = qr.link;
       } else if (qr.q_type === 'externalURL' && qr.link) {
+        console.log('link')
         // Redirect to an external URL
-        window.location.href = qr.link;
+        // window.location.href = qr.link;
       } else {
         // Default fallback, redirect to dashboard or error page
-        router.push('/dashboard');
+        // router.push('/dashboard');
       }
     } else {
       console.error('QR code not found for UUID:', uuid);
